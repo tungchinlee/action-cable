a84899e06894c066d757f5d0897737e9ecf3c1cb
tungchinlee <tungchinli@gmail.com>
2020-10-23 17:30:33 +0800

    model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,20 +7,21 @@ RSpec.describe User, type: :model do
     expect(user).not_to be_valid
   end
 
-  it 'it is not vaild without a email' do
-    user = User.new(email: nil)
-    expect(user).not_to be_valid
-  end
-
-  it 'it is not vaild without a password' do
-    user = User.new(encrypted_password: nil)
-    expect(user).not_to be_valid
-  end
-
   it 'has many messages' do
-    user = User.create(username: 'U-1')
-    message1 = Message.create(user_id: user.id)
-    message2 = Message.create(user_id: user.id)
+    user = User.create(username: 'U-1', email: '123123@123', password: '123123')
+    chatroom = Chatroom.create
+    message1 = Message.create(user_id: user.id, chatroom_id: chatroom.id)
+    message2 = Message.create(user_id: user.id, chatroom_id: chatroom.id)
     expect(user.messages).to include(message1, message2)
   end
+
+  it 'has many chatroom' do
+    user = User.create(username: 'U-1', email: '123123@123', password: '123123')
+    chatroom1 = Chatroom.create
+    chatroom2 = Chatroom.create
+    cuatroom_users = ChatroomUser.create(user_id: user.id, chatroom_id: chatroom1.id)
+    cuatroom_users = ChatroomUser.create(user_id: user.id, chatroom_id: chatroom2.id)
+    expect(user.chatrooms).to include(chatroom1, chatroom2)
+  end
+  
 end
